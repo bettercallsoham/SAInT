be759e0ef1a84cc92e3115607b3e48acbc59b364
Soham <rimatip472@operades.com>
2025-06-27 22:25:01 +0530

    pushed to main

---
+++ SAInT-website/src/App.jsx
@@ -0,0 +1,13 @@
+import EventsPage from './components/EventsPage';
+import './index.css'; 
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <EventsPage />
+    </div>
+  )
+}
+
+export default App
